9816a43931afbc043512c163ca38c90a384c6ff8
Gabriel Menezes <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2022-10-17 21:11:09 -0300

    feat: create Dashboard component + styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { GlobalStyle } from "./styles/global";
+import { GlobalStyle } from './styles/global';
 
-import Header from "./Components/Header";
+import Header from './Components/Header';
+import Dashboard from './Components/Dashboard';
 
 export function App() {
   return (
     <div className="App">
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </div>
   );
