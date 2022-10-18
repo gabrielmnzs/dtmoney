b75581de2174b8bb1a2f990f60959a7ceb31447b
Gabriel Menezes <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2022-10-17 21:09:49 -0300

    feat: add color green in global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,10 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
     --blue-light: #6933ff;
     --text-title: #363f5f;
     --text-body: #969cb3;
