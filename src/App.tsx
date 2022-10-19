a41400c93867bc5cb5bc30c931779d52213aa44a
Gabriel Menezes <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2022-10-18 23:51:30 -0300

    feat: add events to manage modal in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+
 import { GlobalStyle } from './styles/global';
 
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
+import NewTransactionModal from './components/NewTransactionModal';
 
 export function App() {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+		useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+
 	return (
 		<div className="App">
-			<Header />
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 			<Dashboard />
+			<NewTransactionModal
+				isOpen={isNewTransactionModalOpen}
+				onRequestClose={handleCloseNewTransactionModal}
+			/>
 			<GlobalStyle />
 		</div>
 	);
